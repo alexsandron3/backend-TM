8bbacf3562dede503b317dcbabec68de3ab08cb6
Alesk <alexsandro060299@outlook.com>
2022-03-31 12:33:47 -0300

    Fix: Fix server crash for prisma at customers controller

--- controllers/customers/index.js
+++ controllers/customers/index.js
@@ -2,35 +2,47 @@ const { StatusCodes } = require('http-status-codes');
 
 const { customer } = require('../../models/');
 
-async function listAll(req, res) {
+async function listAll(req, res, next) {
   const { ocultarInativos } = req.query;
-  const customers = await customer.listAll(ocultarInativos);
-  return res.status(StatusCodes.OK).json({
-    customers,
-    success: 1,
-    message: 'Pesquisa realizada com sucesso!',
-  });
+  try {
+    const customers = await customer.listAll(ocultarInativos);
+    return res.status(StatusCodes.OK).json({
+      customers,
+      success: 1,
+      message: 'Pesquisa realizada com sucesso!',
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
-async function listByText(req, res) {
+async function listByText(req, res, next) {
   const { wordToSearch } = req.params;
-  const customers = await customer.listByText(wordToSearch);
-  return res.status(StatusCodes.OK).json({
-    customers,
-    success: 1,
-    message: 'Pesquisa realizada com sucesso!',
-  });
+  try {
+    const customers = await customer.listByText(wordToSearch);
+    return res.status(StatusCodes.OK).json({
+      customers,
+      success: 1,
+      message: 'Pesquisa realizada com sucesso!',
+    });
+  } catch (error) {
+    next(error);
+  }
 }
-async function listById(req, res) {
+async function listById(req, res, next) {
   const { id } = req.params;
-  const customers = await customer.listById(id);
-  return res.status(StatusCodes.OK).json({
-    customers,
-    success: 1,
-    message: 'Pesquisa realizada com sucesso!',
-  });
+  try {
+    const customers = await customer.listById(id);
+    return res.status(StatusCodes.OK).json({
+      customers,
+      success: 1,
+      message: 'Pesquisa realizada com sucesso!',
+    });
+  } catch (error) {
+    next(error);
+  }
 }
-async function create(req, res) {
+async function create(req, res, next) {
   const {
     nomeCliente,
     emailCliente,
@@ -55,36 +67,39 @@ async function create(req, res) {
     poltrona,
     statusCpf,
   } = req.body;
-  const customers = await customer.create({
-    nomeCliente,
-    emailCliente,
-    rgCliente,
-    orgaoEmissor,
-    cpfCliente,
-    telefoneCliente,
-    dataNascimento,
-    idadeCliente,
-    referencia,
-    pessoaContato,
-    telefoneContato,
-    cpfConsultado,
-    dataCpfConsultado,
-    redeSocial,
-    statusCliente,
-    enderecoCliente,
-    nacionalidade,
-    profissao,
-    estadoCivil,
-    clienteRedeSocial,
-    poltrona,
-    statusCpf,
-  });
-
-  return res.status(StatusCodes.CREATED).json({
-    customers,
-    success: 1,
-    message: 'Cliente criado com sucesso!',
-  });
+  try {
+    const customers = await customer.create({
+      nomeCliente,
+      emailCliente,
+      rgCliente,
+      orgaoEmissor,
+      cpfCliente,
+      telefoneCliente,
+      dataNascimento,
+      idadeCliente,
+      referencia,
+      pessoaContato,
+      telefoneContato,
+      cpfConsultado,
+      dataCpfConsultado,
+      redeSocial,
+      statusCliente,
+      enderecoCliente,
+      nacionalidade,
+      profissao,
+      estadoCivil,
+      clienteRedeSocial,
+      poltrona,
+      statusCpf,
+    });
+    return res.status(StatusCodes.CREATED).json({
+      customers,
+      success: 1,
+      message: 'Cliente criado com sucesso!',
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
