48713f89576cbd89bef6d552c2347f5a9aa0ebe7
Alesk <alexsandro060299@outlook.com>
2022-04-04 20:58:10 -0300

    Refact: Refact users  model to use the default prisma class

--- models/users/findUserByUsername.js
+++ models/users/findUserByUsername.js
@@ -1,6 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
+const prisma = require('../../utils/prismaClient');
 module.exports = async (username) => {
   return await prisma.users.findFirst({ where: { username } });
 };
